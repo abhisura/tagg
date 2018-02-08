d89fa5a536013e848e60f5ccbdf503cae90655c4
mandeep <mandeepsingh@unomaha.edu>
2018-02-07 20:39:51 -0600

    request pending error fix on business user dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,9 +5,7 @@
  * Date: 9/30/2017
  * Time: 9:51 PM
  */
-
 namespace App\Http\Controllers;
-
 use App\Custom\Constant;
 use App\DonationRequest;
 use App\Organization;
@@ -16,18 +14,14 @@ use App\User;
 use Auth;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
         if ($request->user()->roleuser->role_id == Constant::ROOT_USER OR $request->user()->roleuser->role_id == Constant::TAGG_ADMIN OR $request->user()->roleuser->role_id == Constant::TAGG_USER) {
             $organizations = Organization::all();
-
             $activeParent = Organization::active()->where('trial_ends_at', '>=', Carbon::now()->toDateTimeString())->pluck('id')->toArray();
-
             $activeOrgIds = ParentChildOrganizations::active()->whereIn('parent_org_id', $activeParent)->pluck('child_org_id')->toArray();
-
             $idCount = count($activeOrgIds);
             foreach ($activeParent as $key => $activeID) {
                 $test = [$idCount => $activeID];
@@ -35,44 +29,32 @@ class DashboardController extends Controller
                 $idCount+= 1;
             }
             $activeLocations = Organization::whereIn('id', $activeOrgIds)->get();
-
             $numActiveLocations = count($activeLocations);
-
             // Only parent organizations have 'trial_ends_at' field in the Organizations table
             $organizationsArray = Organization::active()->where('trial_ends_at', '>=', Carbon::now()->toDateTimeString())->pluck('id')->toArray();
             // Counting the number of parent organizations
             $userCount = count($organizationsArray);
-
             $userThisWeek = Organization::active()->where('created_at', '>=', Carbon::now()->startOfWeek())->whereNotNull('trial_ends_at')->count();
             $userThisMonth = Organization::active()->where('created_at', '>=', Carbon::now()->startOfMonth())->whereNotNull('trial_ends_at')->count();
             $userThisYear = Organization::active()->where('created_at', '>=', Carbon::now()->startOfYear())->whereNotNull('trial_ends_at')->count();
-
             $avgAmountDonated = sprintf("%.2f", (DonationRequest::where('approval_status_id', Constant::APPROVED)->avg('approved_dollar_amount')));
-
             $rejectedNumber = DonationRequest::where('approval_status_id', Constant::REJECTED)->count();
             $approvedNumber = DonationRequest::where('approval_status_id', Constant::APPROVED)->count();
             $pendingNumber = DonationRequest::whereIn('approval_status_id', [Constant::PENDING_REJECTION, Constant::PENDING_APPROVAL])->count();
-
             return view('dashboard.admin-index', compact('organizations', 'avgAmountDonated', 'rejectedNumber', 'approvedNumber', 'pendingNumber', 'numActiveLocations', 'userCount', 'userThisWeek', 'userThisMonth', 'userThisYear'));
         } else {
             $organizationId = Auth::user()->organization_id;
             $organization = Organization::findOrFail($organizationId);
-
             $organizationName = $organization->org_name;
-
             $donationrequests = DonationRequest::whereIn('organization_id', $this->getAllMyOrganizationIds())
                 ->whereIn('approval_status_id', [Constant::SUBMITTED, Constant::PENDING_REJECTION, Constant::PENDING_APPROVAL])->get();
             $amountDonated = DonationRequest::where('approval_status_id', Constant::APPROVED)->where('approved_organization_id', $organizationId)->sum('approved_dollar_amount');
             $rejectedNumber = DonationRequest::where('approval_status_id', Constant::REJECTED)->where('approved_organization_id', $organizationId)->count();
             $approvedNumber = DonationRequest::where('approval_status_id', Constant::APPROVED)->where('approved_organization_id', $organizationId)->count();
-            $pendingNumber = DonationRequest::whereIn('approval_status_id', [Constant::PENDING_REJECTION, Constant::PENDING_APPROVAL])->where('approved_organization_id', $organizationId)->count();
-
+            $pendingNumber = DonationRequest::whereIn('approval_status_id', [Constant::PENDING_REJECTION, Constant::PENDING_APPROVAL])-> count(); //where('approved_organization_id', $organizationId)->count();
             return view('dashboard.index', compact('donationrequests', 'organizationName', 'amountDonated', 'rejectedNumber', 'approvedNumber', 'pendingNumber'));
         }
-
-
     }
-
     protected function getAllMyOrganizationIds()
     {
         $organizationId = Auth::user()->organization->id;
@@ -81,4 +63,17 @@ class DashboardController extends Controller
         return $orgIds;
     }
 
+    public function destroy($id)
+    {
+        if (in_array($id, $this->getAllMyOrganizationIds())) {
+            $organization = Organization::find($id);
+            $organization->active = Constant::INACTIVE;
+            $organization->save();
+            $users = User::active()->where('organization_id', $id);
+            $users->update(['active' => Constant::INACTIVE]);
+            return redirect()->back()->with('message', 'Successfully deactivated the Business Location');
+        } else {
+            return redirect('organizations')->withErrors(array('0' => 'You do not have access to remove this Business!!'));
+        }
+    }
 }
